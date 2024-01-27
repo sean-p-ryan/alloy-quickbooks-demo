8074853a6d0529a96d13c614ac7092ae36075bf5
Sean Ryan <seanryan@Seans-MacBook-Pro.local>
2024-01-26 20:28:20 -0500

    Added form for username

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/api', (req, res) => {
+  res.json({ message: "Hello from Express!" });
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
